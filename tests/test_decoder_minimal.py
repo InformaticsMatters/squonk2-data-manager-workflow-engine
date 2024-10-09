3d3e2bd0744db3203607ec9c397b784ee858cc43
Alan Christie <alan.christie@matildapeak.com>
2024-10-09 21:03:24 +0200

    feat: Initial (basic) test

---
+++ tests/test_decoder_minimal.py
@@ -0,0 +1,27 @@
+# Tests for the decoder package.
+from typing import Any, Dict
+
+import pytest
+
+pytestmark = pytest.mark.unit
+
+from workflow import decoder
+
+
+# A minimal Workflow Definition.
+# Tests can use this and adjust accordingly.
+_MINIMAL: Dict[str, Any] = {
+    "kind": "DataManagerWorkflow",
+    "kind-version": "2024.1",
+    "name": "test-minimal",
+}
+
+
+def test_validate_minimal():
+    # Arrange
+
+    # Act
+    error = decoder.validate_schema(_MINIMAL)
+
+    # Assert
+    assert error is None
