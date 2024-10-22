ce1ad330498bfb9aead26814992680d0606389c0
Alan Christie <alan.christie@matildapeak.com>
2024-10-22 18:01:01 +0200

    feat: Initial workflow engine stub

--- workflow/workflow_abc.py
+++ workflow/workflow_abc.py
@@ -17,6 +17,34 @@ class LaunchResult:
     task_id: Optional[str]
 
 
+class InstanceLauncher(ABC):
+    """The class handling the launching of (Job) instances."""
+
+    @abstractmethod
+    def launch(
+        self,
+        *,
+        project_id: str,
+        workflow_id: str,
+        workflow_definition: Dict[str, Any],
+        step: str,
+        step_specification: Dict[str, Any],
+    ) -> LaunchResult:
+        """Launch a (Job) Instance"""
+
+        # launch() provides the instance launcher with sufficient information
+        # to not only create an instance but also create any RunningWorkflow
+        # and RunningWorkflowStep records. The WE must identify the step to run
+        # and then render the specification (using the DM Job Decoder) using
+        # workflow parameters and workflow input and output connections.
+        #
+        # A lot of logic will need to be 'refactored' and maybe the launcher()
+        # needs to render the specification based on variables injected into the
+        # step_specification by the WE? Remember that we have to deal with
+        # "input Handlers" that manipulate the specification variables.
+        # See _instance_preamble() in the DM's api_instance.py module.
+
+
 class DatabaseAdapter(ABC):
     """The DatabaseAdapter will not need to provide a save/commit service.
     Instead it should be assumed that new database records are the
