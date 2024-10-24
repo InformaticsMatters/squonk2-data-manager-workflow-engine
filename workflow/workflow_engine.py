ca798c8ac246f8e54ed86db5e90c47a1a5b8f347
Alan Christie <alan.christie@matildapeak.com>
2024-10-24 13:52:22 +0200

    fix: Handler no longer returns a result

--- workflow/workflow_engine.py
+++ workflow/workflow_engine.py
@@ -1,25 +1,9 @@
 """The WorkflowEngine execution logic.
 """
 
-from dataclasses import dataclass
-from typing import Optional
-
-from informaticsmatters.protobuf.datamanager.pod_message_pb2 import PodMessage
+from google.protobuf.message import Message
 
 from .workflow_abc import DatabaseAdapter, InstanceLauncher
-
-
-@dataclass
-class HandlePodMessageResult:
-    """WorkflowEngine handle message result."""
-
-    error: int
-    error_msg: Optional[str]
-
-
-_SUCCESS_MESSAGE_RESULT: HandlePodMessageResult = HandlePodMessageResult(
-    error=0, error_msg=None
-)
 
 
 class WorkflowEngine:
@@ -38,18 +22,16 @@ class WorkflowEngine:
         self._instance_launcher = instance_launcher
         self._db_adapter = db_adapter
 
-    def handle_pod_message(
-        self,
-        pod_msg: PodMessage,
-    ) -> HandlePodMessageResult:
-        """Given a PodMessage, we use it to identify the Pod (Instance) exit code,
+    def handle_message(self, msg: Message) -> None:
+        """Given a Pod Message, we use it to identify the Pod (Instance) exit code,
         workflow and step and decide what to do next.
 
         Only pod messages relating to workflow instances will be delivered to this method.
         The Pod message has an 'instance' property that provides the UUID of
         the instance that was run. This can be used to correlate the instance with the
         running workflow step.
-        """
-        assert pod_msg
 
-        return _SUCCESS_MESSAGE_RESULT
+        Additionally we will encounter WorkflowMessages that signal the need to
+        start and stop workflows.
+        """
+        assert msg
