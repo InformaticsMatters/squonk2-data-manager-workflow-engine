539365371a3aaa08b41beed1adf8726329ab50ed
Alan Christie <alan.christie@matildapeak.com>
2025-03-11 14:33:18 +0100

    feat: Adjust validator

--- tests/test_worflow_validator.py
+++ tests/test_worflow_validator.py
@@ -2,32 +2,19 @@ import pytest
 
 pytestmark = pytest.mark.unit
 
-from tests.message_dispatcher import UnitTestMessageDispatcher
-from tests.message_queue import UnitTestMessageQueue
 from tests.test_decoder_minimal import _MINIMAL_WORKFLOW
-from tests.wapi_adapter import UnitTestWorkflowAPIAdapter
 from workflow.worklfow_validator import ValidationLevel, WorkflowValidator
 
 
-@pytest.fixture
-def basic_validator():
-    # A 'basic' unit-test WorkflowAdapter needs a DB adapter and Message Dispatcher.
-    # For testing outside of the DM the Message Dispatcher also needs a Message Queue
-    wapi_adapter = UnitTestWorkflowAPIAdapter()
-    msg_queue = UnitTestMessageQueue()
-    msg_dispatcher = UnitTestMessageDispatcher(msg_queue=msg_queue)
-    return WorkflowValidator(wapi_adapter=wapi_adapter, msg_dispatcher=msg_dispatcher)
-
-
-def test_validate_minimal_for_create(basic_validator):
+def test_validate_minimal_for_create():
     # Arrange
 
     # Act
-    error = basic_validator.validate(
+    error = WorkflowValidator.validate(
         level=ValidationLevel.CREATE,
         workflow_definition=_MINIMAL_WORKFLOW,
     )
 
     # Assert
-    assert error.error == 0
+    assert error.error_num == 0
     assert error.error_msg is None
--- workflow/worklfow_validator.py
+++ workflow/worklfow_validator.py
@@ -5,7 +5,6 @@ from enum import Enum
 from typing import Any
 
 from .decoder import validate_schema
-from .workflow_abc import MessageDispatcher, WorkflowAPIAdapter
 
 
 class ValidationLevel(Enum):
@@ -20,30 +19,12 @@ class ValidationLevel(Enum):
 class ValidationResult:
     """Workflow validation results."""
 
-    error: int
+    error_num: int
     error_msg: list[str] | None
 
 
-@dataclass
-class StartResult:
-    """WorkflowEngine start workflow result."""
-
-    error: int
-    error_msg: str | None
-    running_workflow_id: str | None
-
-
-@dataclass
-class StopResult:
-    """WorkflowEngine stop workflow result."""
-
-    error: int
-    error_msg: str | None
-
-
 # Handy successful results
-_VALIDATION_SUCCESS = ValidationResult(error=0, error_msg=None)
-_SUCCESS_STOP_RESULT: StopResult = StopResult(error=0, error_msg=None)
+_VALIDATION_SUCCESS = ValidationResult(error_num=0, error_msg=None)
 
 
 class WorkflowValidator:
@@ -51,16 +32,9 @@ class WorkflowValidator:
     to check workflow content prior to creation and execution.
     """
 
-    def __init__(
-        self, *, wapi_adapter: WorkflowAPIAdapter, msg_dispatcher: MessageDispatcher
-    ):
-        assert wapi_adapter
-
-        self._wapi_adapter = wapi_adapter
-        self._msg_dispatcher = msg_dispatcher
-
+    @classmethod
     def validate(
-        self,
+        cls,
         *,
         level: ValidationLevel,
         workflow_definition: dict[str, Any],
@@ -74,42 +48,6 @@ class WorkflowValidator:
             assert isinstance(workflow_inputs, dict)
 
         if error := validate_schema(workflow_definition):
-            return ValidationResult(error=1, error_msg=[error])
+            return ValidationResult(error_num=1, error_msg=[error])
 
         return _VALIDATION_SUCCESS
-
-    def start(
-        self,
-        *,
-        project_id: str,
-        workflow_id: str,
-        workflow_definition: dict[str, Any],
-        workflow_parameters: dict[str, Any],
-    ) -> StartResult:
-        """Called to initiate workflow by finding the first Instance (or instances)
-        to run and then launching them. It is used from the API Pod, and apart from
-        validating the workflow definition for suitability it sends a Start message
-        to the internal message bus.
-        """
-        assert project_id
-        assert workflow_id
-        assert workflow_definition
-        assert workflow_parameters
-
-        return StartResult(
-            error=0,
-            error_msg=None,
-            running_workflow_id="r-workflow-6aacd971-ca87-4098-bb70-c1c5f19f4dbf",
-        )
-
-    def stop(
-        self,
-        *,
-        running_workflow_id: str,
-    ) -> StopResult:
-        """Stop a running workflow. It is used from the API Pod, and apart from
-        validating the workflow definition for suitability it sends a Stop message
-        to the internal message bus."""
-        assert running_workflow_id
-
-        return _SUCCESS_STOP_RESULT
