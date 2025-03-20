03769740c19358793fa4552c971bdfad631be19e
Alan Christie <alan.christie@matildapeak.com>
2025-03-20 17:57:48 +0100

    fix: Removed workflow variables (not needed)

---
+++ tests/jobs/addcol.py
@@ -0,0 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="addcol",
+    description="Takes a SMILES string and writes it to a file",
+)
+parser.add_argument("-i", "--inputFile", required=True)
+parser.add_argument("-o", "--outputFile", required=True)
+parser.add_argument("-n", "--name", required=True)
+parser.add_argument("-v", "--value", required=True)
+args = parser.parse_args()
+
+with open(args.inputFile, "rt", encoding="utf8") as input_file:
+    content = input_file.read()
+    with open(args.outputFile, "wt", encoding="utf8") as output_file:
+        output_file.write(content)
